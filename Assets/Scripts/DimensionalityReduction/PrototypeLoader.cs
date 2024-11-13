7b39c51611b168c18368bd937ed946cb39519b5e
Florian Spiess <florian.spiess@unibas.ch>
2024-11-13 13:52:00 +0100

    Add randomized previews and multiple interactor previews

--- Assets/Scripts/DimensionalityReduction/PrototypeLoader.cs
+++ Assets/Scripts/DimensionalityReduction/PrototypeLoader.cs
@@ -1,8 +1,8 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Globalization;
 using System.Linq;
+using SimpleInteractionSystem;
 using UnityEngine;
 using UnityEngine.Networking;
 
@@ -38,17 +38,24 @@ namespace DimensionalityReduction
   public class PrototypeLoader : MonoBehaviour
   {
     public ParticleSystem system;
-    public Transform previewPosition;
-    public TextMesh previewText;
-    public Renderer previewImage;
+    public Renderer previewPrefab;
     public float previewScale = 0.2f;
     public Coloration startColor;
 
     [Tooltip("Maximum distance (squared) to point such that thumbnail is still displayed")]
     public float maximumDistanceSquared = 0.01f;
 
+    [Header("Randomized Previews")] public bool enableRandomizedPreviews = true;
+
+    [Tooltip("Minimum distance between previews to control sparsity")]
+    public float minimumPreviewDistance = .18f;
+
+    public float randomizedPreviewScale = .1f;
+    public float hideRandomizedPreviewDistanceSquared = 0.02f;
+
+    private Dictionary<Vector3, Renderer> _randomizedPreviews = new();
+
     private List<(string id, Vector3 position)> _points;
-    private string _lastSelected = "";
 
     // Interaction variables
     private Dictionary<Transform, Vector3> _activeInteractors = new();
@@ -56,6 +63,10 @@ namespace DimensionalityReduction
 
     // Config
     private Server _dataServer = Server.Sipi;
+
+    // For tracking the interactors currently inside the bounding box and the last segment data they hovered.
+    private readonly Dictionary<Interactor, string> _enteredLastId = new();
+    private readonly Dictionary<Interactor, Renderer> _enteredPreviews = new();
 
     private void Start()
     {
@@ -69,6 +80,61 @@ namespace DimensionalityReduction
       _points = items;
 
       EmitParticles(startColor);
+
+      if (enableRandomizedPreviews)
+        GeneratePreviews();
+    }
+
+    private void GeneratePreviews()
+    {
+      // Destroy existing previews
+      foreach (var (_, preview) in _randomizedPreviews)
+      {
+        Destroy(preview.gameObject);
+      }
+
+      _randomizedPreviews.Clear();
+
+      // Randomly choose points to display previews for
+      var orderedPoints = _points.OrderBy(_ => UnityEngine.Random.value).ToList();
+
+      var previewPoints = new List<(string id, Vector3 position)>();
+
+      var sqrMinimumPreviewDistance = minimumPreviewDistance * minimumPreviewDistance;
+
+      foreach (var (segment, position) in orderedPoints)
+      {
+        if (previewPoints.Count == 0)
+        {
+          previewPoints.Add((segment, position));
+          continue;
+        }
+
+        var minSqrDistance = previewPoints.Select(pair => (pair.position - position).sqrMagnitude).Min();
+
+        // Ignore points too close to existing previews
+        if (minSqrDistance < sqrMinimumPreviewDistance)
+          continue;
+
+        previewPoints.Add((segment, position));
+      }
+
+      // Create previews
+      _randomizedPreviews = previewPoints.Select(pair =>
+      {
+        var preview = Instantiate(previewPrefab);
+        preview.transform.localScale = previewScale * Vector3.one;
+
+        return (preview, pair.position);
+      }).ToDictionary(pair => pair.position, pair => pair.preview);
+
+      // Start texture download (must occur after previews are created)
+      foreach (var (id, position) in previewPoints)
+      {
+        var thumbnailURL = GetThumbnailURL(id);
+
+        StartCoroutine(DownloadTexture(thumbnailURL, id, position, null, OnDownloadSuccessRandomizedPreview));
+      }
     }
 
     private void Update()
@@ -78,7 +144,34 @@ namespace DimensionalityReduction
 
     private void FixedUpdate()
     {
-      UpdatePreview();
+      UpdatePreviews();
+    }
+    
+    private void OnTriggerEnter(Collider other)
+    {
+      if (!other.TryGetComponent<Interactor>(out var interactor)) return;
+
+      // Check if interactor had previously entered but not been removed
+      if (!_enteredLastId.TryAdd(interactor, null))
+      {
+        _enteredLastId[interactor] = null;
+        return;
+      }
+
+      var preview = Instantiate(previewPrefab);
+      preview.transform.localScale = previewScale * Vector3.one;
+      _enteredPreviews.Add(interactor, preview);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+      if (!other.TryGetComponent<Interactor>(out var interactor)) return;
+
+      _enteredLastId.Remove(interactor);
+
+      var preview = _enteredPreviews[interactor];
+      Destroy(preview.gameObject);
+      _enteredPreviews.Remove(interactor);
     }
 
     public void OnInteraction(Transform interactor, bool start)
@@ -117,6 +210,10 @@ namespace DimensionalityReduction
     {
       system.Clear();
 
+      // Scale particles by size to allow scaling of point cloud for easier selection and viewing, but do not scale up
+      // beyond a limit to prevent small displays becoming crowded
+      var size = .01f / Mathf.Max(transform.localScale.x, 1);
+
       switch (coloration)
       {
         case Coloration.White:
@@ -125,7 +222,7 @@ namespace DimensionalityReduction
                      position = item.position,
                      velocity = Vector3.zero,
                      startLifetime = float.PositiveInfinity,
-                     startSize = .01f,
+                     startSize = size,
                      startColor = Color.white
                    }))
           {
@@ -139,7 +236,7 @@ namespace DimensionalityReduction
                      position = item.position,
                      velocity = Vector3.zero,
                      startLifetime = float.PositiveInfinity,
-                     startSize = .01f,
+                     startSize = size,
                      startColor = new Color(item.position.x + 0.5f, item.position.y + 0.5f, item.position.z + 0.5f)
                    }))
           {
@@ -174,6 +271,8 @@ namespace DimensionalityReduction
       {
         _activeInteractors[key] = key.position;
       }
+
+      EmitParticles(startColor);
     }
 
     /// <summary>
@@ -181,28 +280,54 @@ namespace DimensionalityReduction
     /// appropriate texture.
     /// Also updates preview location and rotation.
     /// </summary>
-    private void UpdatePreview()
+    private void UpdatePreviews()
     {
-      var position = transform.InverseTransformPoint(previewPosition.position);
-
-      var (id, sqrDistance, itemPosition) = _points
-        .Select(item => (item.id, (item.position - position).sqrMagnitude, item.position))
-        .Aggregate((a, b) => a.sqrMagnitude > b.sqrMagnitude ? b : a);
-
-      previewText.text = sqrDistance.ToString(CultureInfo.InvariantCulture);
-
-      previewImage.enabled = sqrDistance < maximumDistanceSquared;
-
-      UpdatePreviewPosRot(itemPosition);
-
-      if (id == _lastSelected)
+      if (_points.Count == 0)
         return;
 
-      var thumbnailURL = GetThumbnailURL(id);
+      // Enable all randomized previews
+      _randomizedPreviews.Values.ToList().ForEach(preview => preview.enabled = true);
 
-      StartCoroutine(DownloadTexture(thumbnailURL, id, itemPosition, OnDownloadSuccess));
+      // Update interactor previews
+      foreach (var interactor in _enteredLastId.Keys.ToList())
+      {
+        var position = transform.InverseTransformPoint(interactor.transform.position);
 
-      _lastSelected = id;
+        var (id, sqrDistance, itemPosition) = _points
+          .Select(item => (item.id, (item.position - position).sqrMagnitude, item.position))
+          .Aggregate((a, b) => a.sqrMagnitude > b.sqrMagnitude ? b : a);
+
+        var preview = _enteredPreviews[interactor];
+
+        var interactorClose = sqrDistance < maximumDistanceSquared / transform.localScale.x;
+
+        preview.enabled = interactorClose;
+
+        if (!interactorClose)
+          continue;
+
+        // Disable all randomized previews closer to the interactor than the minimum distance
+        _randomizedPreviews.Where(pair =>
+            (pair.Key - position).sqrMagnitude < hideRandomizedPreviewDistanceSquared / transform.localScale.x)
+          .ToList().ForEach(pair => pair.Value.enabled = false);
+
+        UpdatePreviewPosRot(itemPosition, preview.transform);
+
+        if (id == _enteredLastId[interactor])
+          continue;
+
+        var thumbnailURL = GetThumbnailURL(id);
+
+        StartCoroutine(DownloadTexture(thumbnailURL, id, itemPosition, interactor, OnDownloadSuccess));
+
+        _enteredLastId[interactor] = id;
+      }
+
+      // Update randomized preview positions and rotations
+      foreach (var (position, preview) in _randomizedPreviews)
+      {
+        UpdatePreviewPosRot(position, preview.transform);
+      }
     }
 
     private string GetThumbnailURL(string id)
@@ -258,9 +383,10 @@ namespace DimensionalityReduction
     /// <param name="url">URL of the image to use as texture</param>
     /// <param name="id">ID of the image to be downloaded for checking relevance</param>
     /// <param name="itemPosition">Position of the item in local space</param>
+    /// <param name="interactor">Interactor causing this preview to be shown</param>
     /// <param name="onSuccess">Function to call when successfully downloaded</param>
-    private static IEnumerator DownloadTexture(string url, string id, Vector3 itemPosition,
-      Action<Texture2D, string, Vector3> onSuccess)
+    private static IEnumerator DownloadTexture(string url, string id, Vector3 itemPosition, Interactor interactor,
+      Action<Texture2D, string, Vector3, Interactor> onSuccess)
     {
       using var www = UnityWebRequestTexture.GetTexture(url);
       yield return www.SendWebRequest();
@@ -271,32 +397,50 @@ namespace DimensionalityReduction
       }
       else
       {
-        var loadedTexture = ((DownloadHandlerTexture) www.downloadHandler).texture;
-        onSuccess(loadedTexture, id, itemPosition);
+        var loadedTexture = ((DownloadHandlerTexture)www.downloadHandler).texture;
+        onSuccess(loadedTexture, id, itemPosition, interactor);
       }
     }
 
-    private void OnDownloadSuccess(Texture2D loadedTexture, string id, Vector3 itemPosition)
+    private void OnDownloadSuccess(Texture2D loadedTexture, string id, Vector3 itemPosition, Interactor interactor)
     {
+      if (!_enteredLastId.ContainsKey(interactor) || id != _enteredLastId[interactor])
+        return;
+
+      var preview = _enteredPreviews[interactor];
       // Set texture
-      previewImage.material.mainTexture = loadedTexture;
+      preview.material.mainTexture = loadedTexture;
       // Adjust aspect ratio
       float factor = Mathf.Max(loadedTexture.width, loadedTexture.height);
       var scale = new Vector3(loadedTexture.width / factor, loadedTexture.height / factor, 1);
-      var t = previewImage.transform;
+      var t = preview.transform;
       t.localScale = scale * previewScale;
-      UpdatePreviewPosRot(itemPosition);
+      UpdatePreviewPosRot(itemPosition, t);
     }
 
-    private void UpdatePreviewPosRot(Vector3 itemPosition)
+    private void OnDownloadSuccessRandomizedPreview(Texture2D loadedTexture, string id, Vector3 itemPosition,
+      Interactor _)
     {
-      var t = previewImage.transform;
+      var preview = _randomizedPreviews[itemPosition];
+      // Set texture
+      preview.material.mainTexture = loadedTexture;
+      // Adjust aspect ratio
+      float factor = Mathf.Max(loadedTexture.width, loadedTexture.height);
+      var scale = new Vector3(loadedTexture.width / factor, loadedTexture.height / factor, 1);
+      var t = preview.transform;
+      t.localScale = scale * randomizedPreviewScale;
+      UpdatePreviewPosRot(itemPosition, t);
+    }
+
+    private void UpdatePreviewPosRot(Vector3 itemPosition, Transform previewTransform)
+    {
       // Set position
-      t.position = transform.TransformPoint(itemPosition) + Vector3.up * (t.localScale.y / 2);
+      previewTransform.position =
+        transform.TransformPoint(itemPosition) + Vector3.up * (previewTransform.localScale.y / 2);
       // Rotate towards camera
-      var forwardVector = t.position - _camera.transform.position;
+      var forwardVector = previewTransform.position - _camera.transform.position;
       forwardVector.y = 0;
-      t.rotation = Quaternion.LookRotation(forwardVector);
+      previewTransform.rotation = Quaternion.LookRotation(forwardVector);
     }
   }
 }
